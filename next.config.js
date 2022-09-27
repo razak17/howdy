ad2b1e1023b254ec9236d54bd96128c9f8167faf
Razak <moabdulrazak17@gmail.com>
2022-09-27 03:35:14 +0000

    feat(components): create video card

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true
+	swcMinify: true,
+	images: {
+		domains: [
+			'lh3.googleusercontent.com',
+			'cdn.pixabay.com',
+			'p16-amd-va.tiktokcdn.com',
+			'image.shutterstock.com',
+			'dergipark.org.tr'
+		]
+	}
 };
 
 module.exports = nextConfig;
