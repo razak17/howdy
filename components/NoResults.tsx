d9a0b47c4b8350f7d45be1331afba61036b822df
Razak <moabdulrazak17@gmail.com>
2022-09-27 03:26:28 +0000

    feat(components): create no results component

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,5 +1,14 @@
+import { MdOutlineVideocamOff } from 'react-icons/md';
+
 const NoResults = ({ text }: { text: string }) => {
-	return <div>NoResults</div>;
+	return (
+		<div className='flex flex-col justify-center items-center h-full w-full'>
+			<p className='text-8xl'>
+				<MdOutlineVideocamOff />
+			</p>
+			<p className='text-2xl text-center'>{text}</p>
+		</div>
+	);
 };
 
 export default NoResults;
