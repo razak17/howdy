3347275a6ebf85abe49e90e89b8d486f670b6027
Razak <moabdulrazak17@gmail.com>
2022-09-27 02:26:04 +0000

    feat(components): create footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,20 @@
-const Footer = () => {
-	return <div>Footer</div>;
-};
+import { footerList1 } from '../lib/constants';
+
+const List = ({ items, mt }: { items: string[]; mt: Boolean }) => (
+	<div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+		{items.map((item: string) => (
+			<p key={item} className='text-gray-400 text-sm  hover:underline cursor-pointer'>
+				{item}
+			</p>
+		))}
+	</div>
+);
+
+const Footer = () => (
+	<div className='mt-6 hidden xl:block'>
+		<List items={footerList1} mt={false} />
+		<p className='text-gray-400 text-sm mt-5'>© 2022 TikTik</p>
+	</div>
+);
 
 export default Footer;
--- lib/constants.tsx
+++ lib/constants.tsx
@@ -46,8 +46,8 @@ export const footerList1 = [
 	'Newsroom',
 	'Store',
 	'Contact',
-	'Carrers',
-	'ByteDance',
+	'Careers',
+	'Privacy Policy',
 	'Creator Directory'
 ];
 export const footerList2 = [
