46d8946c158e7eac653635f7163585945267024b
Razak <moabdulrazak17@gmail.com>
2022-09-27 03:18:09 +0000

    feat(api): fetch posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
-import type { NextPage } from 'next';
 import { getTopic, getPosts } from '../lib/api';
+import { Video } from '../lib/types';
 
-const Home: NextPage = () => {
-	return <div className='text-3xl font-bold underline'>Hello Mom!</div>;
+const Home = ({ videos }: { videos: Video[] }) => {
+	console.log({ videos });
+
+	return (
+		<div className='flex flex-col gap-10 videos h-full'>
+			<h1>Hello Mom!</h1>
+		</div>
+	);
 };
 
 export default Home;
 
 export const getServerSideProps = async ({ query: { topic } }: { query: { topic: string } }) => {
-	let res = getPosts();
+	let res = await getPosts();
 
-	if (topic) {
-		res = getTopic(topic);
-	}
+	// if (topic) {
+	// 	res = getTopic(topic);
+	// }
 
 	return {
 		props: { videos: res }
