daa5454834f23be978a65a31d428ba0c7527e090
Razak <moabdulrazak17@gmail.com>
2022-09-28 00:37:15 +0000

    feat(pages): custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+	return (
+		<div className='flex justify-center items-center h-[100vh]'>
+			<h1 className='m-auto font-normal text-3xl'>404 | That Page Does Not Exist!</h1>
+		</div>
+	);
+};
+
+export default NotFoundPage;
